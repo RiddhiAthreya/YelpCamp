f7814f1f1d2ab66641932b2376c648e12eaef330
Riddhi <riddhi400@gmail.com>
2019-07-21 22:49:44 +0530

    add databaseurl env variable

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var commentRoutes=require("./routes/comments"),
     indexRoutes=require("./routes/index");    
 
 //mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
-mongoose.connect("mongodb+srv://riddhi400:riddhi123@yelpcamp-mrmdo.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
